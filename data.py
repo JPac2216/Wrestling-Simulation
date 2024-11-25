c39ff924a3ef1c02c7b77c58b42764d9aeef2942
YousefHazana347 <gaminggod4567@gmail.com>
2024-11-25 16:08:00 -0500

    imported csv file with data and implemented it into the code

--- data.py
+++ data.py
@@ -3,3 +3,6 @@ import csv
 with open('wwemen.csv', mode='r') as file:
     reader = csv.DictReader(file)
     data = [row for row in reader]
+
+for wrestler in data:
+    print(f"{wrestler['Superstar']}'s theme song is {wrestler['Theme Song']}.")
