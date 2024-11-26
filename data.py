8c3ddc6328fbe12786e656f47962d373a7568b86
JPac2216 <jakepaccione@gmail.com>
2024-11-25 20:02:47 -0500

    linked data.py and index.html

--- data.py
+++ data.py
@@ -1,8 +1,20 @@
+from flask import Flask, render_template, request
 import csv
 
-with open('wwemen.csv', mode='r') as file:
-    reader = csv.DictReader(file)
-    data = [row for row in reader]
+app = Flask(__name__)
 
-for wrestler in data:
-    print(f"{wrestler['Superstar']}'s theme song is {wrestler['Theme Song']}.")
+@app.route("/")
+
+def index():
+    data = []
+    with open('wwemen.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+
+    #for wrestler in data:
+    #   print(f"{wrestler['Superstar']}'s theme song is {wrestler['Theme Song']}.")
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
